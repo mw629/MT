effbfcbb6c07602c079ea435530c3020f97d1d03
聖人 渡辺 <k024g0024@g.neec.ac.jp>
2025-04-10 23:42:58 +0900

    プロジェクト ファイルを追加します。

---
+++ Calculation.h
@@ -0,0 +1,228 @@
+
+///変数型
+
+struct Vecto3 {
+	float x;
+	float y;
+	float z;
+	float w = 1.0f;
+
+	Vecto3 operator+(const Vecto3& other) { return { x + other.x, y + other.y,z + other.z }; }
+	Vecto3 operator-(const Vecto3& other) { return { x - other.x, y - other.y,z - other.z }; }
+	Vecto3 operator*(float other) const { return { x * other, y * other,z * other }; }
+	Vecto3 operator/(const Vecto3& other) { return { x / other.x, y / other.y ,z / other.z }; }
+	Vecto3 operator=(const Vecto3& other) { return { x = other.x, y = other.y,z = other.z }; }
+	Vecto3 operator+=(const Vecto3& other) { return { x += other.x, y += other.y,z+= other.z }; }
+	Vecto3 operator-=(const Vecto3& other) { return { x -= other.x, y -= other.y,z -= other.z }; }
+	Vecto3 operator*=(const Vecto3& other) { return { x *= other.x, y *= other.y,z *= other.z }; }
+	Vecto3 operator/=(const Vecto3& other) { return { x /= other.x, y /= other.y,z /= other.z }; }
+};
+
+struct Matrix4x4 {
+	float m[4][4] = {
+		{0,0,0,0},
+		{0,0,0,0},
+		{0,0,0,0},
+		{0,0,0,0},
+	};
+
+};
+
+
+//三次元ベクトルの計算
+
+
+/// <summary>
+/// この関数は、2つのベクトルの和を計算します。
+/// </summary>
+/// <param name="v1">ベクトル1</param>
+/// <param name="v2">ベクトル2</param>
+///  <returns>ベクトルの各要素を足し合わせた結果を返します。 </returns>
+Vecto3 AddVecto3(Vecto3 v1, Vecto3 v2);
+
+/// <summary>
+/// この関数は、2つのベクトルの差を計算します。
+/// </summary>
+/// <param name="v1">ベクトル1</param>
+/// <param name="v2">ベクトル2</param>
+///  <returns>ベクトルの各要素を引いた結果を返します。 </returns>
+Vecto3 SubtractVector3(Vecto3 v1, Vecto3 v2);
+
+/// <summary>
+/// この関数は二つのベクトルの積を計算します
+/// </summary>
+/// <param name="v1">ベクトル１</param>
+/// <param name="v2">ベクトル２</param>
+/// <returns>ベクトルの各要素を掛けた結果を返します</returns>
+Vecto3 MultiplyVector3(Vecto3 v1, Vecto3 v2);
+
+/// <summary>
+/// この関数は、指定されたスカラー倍をベクトルの各要素に適用します。
+/// </summary>
+/// <param name="v">ベクトル</param>
+/// <param name="s">スカラー値</param>
+/// <returns>スカラー倍された新しいベクトル</returns>
+Vecto3 ScalarMultiply(Vecto3 v, float s);
+
+/// <summary>
+/// この関数は、ベクトルの長さを計算します。
+/// </summary>
+/// <param name="v">ベクトル</param>
+/// <returns>ベクトルの長さ</returns>
+float Norm(Vecto3 v);
+
+/// <summary>
+/// この関数は、ベクトルを正規化します。
+/// </summary>
+/// <param name="v">ベクトル</param>
+/// <returns>正規化されたベクトル</returns>
+Vecto3 Normalize(Vecto3 v);
+
+/// <summary>
+/// この関数は、2つのベクトルの内積を計算します。
+/// </summary>
+/// <param name="v1">ベクトル1</param>
+/// <param name="v2">ベクトル2</param>
+/// <returns>内積の結果</returns>
+float Dot(Vecto3 v1, Vecto3 v2);
+
+
+//行列の計算
+
+/// <summary>
+/// この関数は、2つの行列の和を計算します。
+/// </summary>
+/// <param name="m1">行列1</param>
+/// <param name="m2">行列2</param>
+/// <returns>2つの行列の和</returns>
+Matrix4x4 AddMatrix4x4(Matrix4x4 m1, Matrix4x4 m2);
+
+
+/// <summary>
+/// この関数は、2つの行列の積を計算します。
+/// </summary>
+/// <param name="m1">行列1</param>
+/// <param name="m2">行列2</param>
+/// <returns>2つの行列の積</returns>
+Matrix4x4 MultiplyMatrix4x4(Matrix4x4 m1, Matrix4x4 m2);
+
+/// <summary>
+/// この関数は、行列にスカラー倍を適用します。
+/// </summary>
+/// <param name="m">行列</param>
+/// <param name="s">スカラー値</param>
+/// <returns>スカラー倍された行列</returns>
+Matrix4x4 ScaleMultiplyMatrix4x4(Matrix4x4 m, float s);
+
+/// <summary>
+/// この関数は、行列の転置を計算します。
+/// </summary>
+/// <param name="m">行列</param>
+/// <returns>転置された行列</returns>
+Matrix4x4 TransposeMatrix4x4(Matrix4x4 m);
+
+/// <summary>
+/// この関数は、行列の逆行列を計算します。
+/// </summary>
+/// <param name="m">行列</param>
+/// <returns>逆行列</returns>
+Matrix4x4 InverseMatrix4x4(Matrix4x4 m);
+
+/// <summary>
+/// この関数は、ベクトルを行列で変換します。
+/// </summary>
+/// <param name="v">ベクトル</param>
+/// <param name="m">行列</param>
+/// <returns>変換されたベクトル</returns>
+Vecto3 Transform(const Vecto3& v, const Matrix4x4& m);
+
+
+///アフィン行列
+
+/// <summary>
+/// この関数は、行列をスケーリングします。
+/// </summary>
+/// <param name="scall">スケーリング値</param>
+/// <returns>スケーリング行列</returns>
+Matrix4x4 Scale(Vecto3 scall);
+
+/// <summary>
+/// この関数は、X軸を中心に回転する行列を計算します。
+/// </summary>
+/// <param name="angle">回転角度</param>
+/// <returns>回転行列</returns>
+Matrix4x4 RotationX(float angle);
+
+/// <summary>
+/// この関数は、Y軸を中心に回転する行列を計算します。
+/// </summary>
+/// <param name="angle">回転角度</param>
+/// <returns>回転行列</returns>
+Matrix4x4 RotationY(float angle);
+
+/// <summary>
+/// この関数は、Z軸を中心に回転する行列を計算します。
+/// </summary>
+/// <param name="angle">回転角度</param>
+/// <returns>回転行列</returns>
+Matrix4x4 RotationZ(float angle);
+
+/// <summary>
+/// この関数は、任意の軸を中心に回転する行列を計算します。
+/// </summary>
+/// <param name="angleX">X軸回転角度</param>
+/// <param name="angleY">Y軸回転角度</param>
+/// <param name="angleZ">Z軸回転角度</param>
+/// <returns>回転行列</returns>
+Matrix4x4 Rotation(Vecto3 angle);//不安
+
+/// <summary>
+/// この関数は、ベクトルを行列で変換します。
+/// </summary>
+/// <param name="pos">位置</param>
+/// <param name="m">行列</param>
+Matrix4x4 Translation(Vecto3 pos);
+
+/// <summary>
+/// この関数は、アフィン行列を作成します。
+/// </summary>
+/// <param name="pos">位置</param>
+/// <param name="scale">スケーリング値</param>
+/// <param name="angle">回転角度</param>
+/// <returns>アフィン行列</returns>
+Matrix4x4 MakeAffineMatrix(Vecto3 pos, Vecto3 scale, Vecto3 angle);
+
+/// <summary>
+/// この関数は、正射影行列を作成します。
+/// </summary>
+/// <param name="l">左</param>
+/// <param name="r">右</param>
+/// <param name="t">上</param>
+/// <param name="b">下</param>
+/// <param name="zn">近クリップ面</param>
+/// <param name="zf">遠クリップ面</param>
+/// <returns>正射影行列</returns>
+Matrix4x4 MakeOrthographicMatrix(float l, float r, float t, float b, float zn, float zf);
+
+/// <summary>
+/// この関数は、透視投影行列を作成します。
+/// </summary>
+/// <param name="fov">視野角</param>
+/// <param name="aspect">アスペクト比</param>
+/// <param name="zn">近クリップ面</param>
+/// <param name="zf">遠クリップ面</param>
+/// <returns>透視投影行列</returns>
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float a, float zn, float zf);
+
+/// <summary>
+/// この関数は、ビューポート行列を作成します。
+/// </summary>
+/// <param name="width">幅</param>
+/// <param name="height">高さ</param>
+/// <param name="left">左</param>
+/// <param name="top">上</param>
+/// <param name="zn">最小深度値</param>
+/// <param name="zf">最大深度値</param>
+/// <returns>ビューポート行列</returns>
+Matrix4x4 MakeViewPortMatrix(float width, float height, float left, float top, float minD, float maxD);
+
---
+++ main.cpp
@@ -0,0 +1,120 @@
+#include <Novice.h>
+#include "Calculation.h"
+
+
+
+const char kWindowTitle[] = "LC1A_27_ワタナベ_マサト";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+
+	
+	Vecto3 cameraPos = { 0.0f, 0.0f, 0.0f};
+	Matrix4x4 cameraMatrix;
+
+	Matrix4x4 viewMatrix;
+	Matrix4x4 projectionMatrix;
+	Matrix4x4 worldViewProjectionMatrix;
+	Matrix4x4 viewportMatrix;
+
+	Vecto3 pos = { 0.0f,0.0f,0.0f };//1.0f,-400.0f,100.0f
+	Vecto3 kLocalVertices[3] =
+	{
+		{10.0f,10.0f,0.0f},
+		{30.0f,10.0f,0.0f},
+		{20.0f,30.0f,0.0f}
+	};
+	Matrix4x4 WorldMatrix;
+
+	// 垂直方向視野角
+	float fovAngleY = 90.0f;
+	// ビューポートのアスペクト比
+	float aspectRatio = (float)720 / 1280;
+	// 深度限界（手前側）
+	float nearZ = 0.1f;
+	// 深度限界（奥側）
+	float farZ = 1.0f;
+
+
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+		
+
+
+		WorldMatrix = MakeAffineMatrix(pos, { 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f });
+		cameraMatrix = MakeAffineMatrix(cameraPos, { 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f });
+
+		viewMatrix = InverseMatrix4x4(cameraMatrix);
+		projectionMatrix = MakePerspectiveFovMatrix(fovAngleY, aspectRatio, nearZ, farZ);
+
+		worldViewProjectionMatrix = MultiplyMatrix4x4(WorldMatrix, MultiplyMatrix4x4(viewMatrix, projectionMatrix));
+
+		viewportMatrix = MakeViewPortMatrix(1280.0f, 720.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+
+		Vecto3 screenVertice[3];
+
+		for (int i = 0; i < 3; i++) {
+			Vecto3 ndcVertice = Transform(kLocalVertices[i], worldViewProjectionMatrix);
+			screenVertice[i] = Transform(ndcVertice, viewportMatrix);
+		}
+
+
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		Novice::DrawTriangle(
+			static_cast<int>(screenVertice[0].x), static_cast<int>(screenVertice[0].y),
+			static_cast<int>(screenVertice[1].x), static_cast<int>(screenVertice[1].y),
+			static_cast<int>(screenVertice[2].x), static_cast<int>(screenVertice[2].y),
+			0xFFFFFFFF, kFillModeSolid);
+
+		for (int i = 0; i < 3; i++)
+		{
+			Novice::ScreenPrintf(10, 10 + i * 20, "screenVertice[%d] = (%f, %f)", i, screenVertice[i].x, screenVertice[i].y);
+		}
+
+
+
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
